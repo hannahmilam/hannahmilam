9871282ecb2b7f872932e7d19687f4ad9ecc4303
Hannah Milam <hannah.milam16@gmail.com>
2021-09-08 07:07:53 -0600

    finished week 1 career homework

--- app/AppState.js
+++ app/AppState.js
@@ -3,7 +3,7 @@ import { isValidProp } from "./Utils/isValidProp.js"
 
 class AppState extends EventEmitter {
   /** @type {import('./Models/Value').Value[]} */
-  values = []
+
 }
 
 export const ProxyState = new Proxy(new AppState(), {
